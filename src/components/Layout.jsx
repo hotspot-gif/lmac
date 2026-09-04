30bfdbe78101b9fbb9afda740bd74f476932063e
Dilan Fernando <hs.lycamobile.it@gmail.com>
2026-09-04 11:56:09 +0200

    UI CHANGE 2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -87,7 +87,9 @@ export default function Layout({ children }) {
             'text-white/70 hover:bg-white/10 hover:text-white'}`
             }>
             
-              <Icon className="w-4 h-4 flex-shrink-0" />
+              <span className="w-6 h-6 rounded-md bg-[#08dc7d] flex items-center justify-center flex-shrink-0">
+                <Icon className="w-4 h-4 text-white" />
+              </span>
               {t(item.label)}
             </Link>);
 
